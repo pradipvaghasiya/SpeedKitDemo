93d615c772d88413e8aa03b0fe271b0593e02b80
Pradip Vaghasiya <pradip.vaghasiya@gmail.com>
2014-07-13 22:26:19 +0530

    Initial Commit

---
+++ SpeedKitDemo/SampleTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SampleTableViewCell.swift
+//  SpeedKitDemo
+//
+//  Created by Pradip Vaghasiya on 13/07/14.
+//  Copyright (c) 2014 Happyfall. All rights reserved.
+//
+
+import UIKit
+
+let kCellIdSampleTableViewCell = "SampleTableViewCell"
+
+class SampleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- SpeedKitDemo/ViewController.swift
+++ SpeedKitDemo/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, SPTableViewControllerProtocol {
+    
+    @IBOutlet var tableView : UITableView
+    var tableViewDataSource : AnyObject[] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.dataSource = self
+        
+        tableViewDataSource = [
+            // Section 0
+            [
+                [
+                    kCellIdKey : kCellIdSampleTableViewCell,
+                    kCellCountKey : 2
+                ],
+                [
+                    kCellIdKey : kCellIdSampleTableViewCell,
+                    kCellCountKey : 3
+                ]
+            ],
+            // Section 1
+            [
+                [
+                    kCellIdKey : kCellIdSampleTableViewCell,
+                    kCellCountKey : 5
+                ]
+            ]];
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func sayHello(){
+        println("Pradip")
+    }
+    
+    
 }
 
