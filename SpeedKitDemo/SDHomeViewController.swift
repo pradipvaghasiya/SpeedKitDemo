5e97097beb77b456a5d6f1bae7ced804bc58b759
Pradip Vaghasiya <pradip.vaghasiya@gmail.com>
2015-04-12 20:34:17 +0530

    Section Header Footer bug fix.

--- SpeedKitDemo/SDHomeViewController.swift
+++ SpeedKitDemo/SDHomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SDHomeViewController.swift
 //  SpeedKitDemo
 //
 //  Created by Pradip Vaghasiya on 09/07/14.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, SPListingViewControllerProtocol {
+class SDHomeViewController: UIViewController {
     
 //    SPListingViewControllerProtocol
     var spListingSectionDataArray : [SPListingSectionData] = []
@@ -31,7 +31,7 @@ class ViewController: UIViewController, SPListingViewControllerProtocol {
 
 //MARK: Add Table View
 
-extension ViewController{
+extension SDHomeViewController{
     func addTableView(){
         if let listingVC = SPListingTableVC.getNewInstance(){
             spListingSectionDataArray = [
@@ -40,23 +40,19 @@ extension ViewController{
                     SPListingCellData(
                         cellId: kCellIdSampleTableViewCell,
                         cellCount: 3,
-                        cellModel:[SampleTableViewCellModel(TitleText: "Sample Data")])
-                    ], sectionHeader: "Section 1", sectionFooter: "Footer 1"),
+                        cellModelCommon:SampleTableViewCellModel(TitleText: "Sample Data"))
+                    ], sectionHeader: "Section 1"),
                 // Section 1
                 SPListingSectionData(Rows: [
                     SPListingCellData(
                         cellId: kCellIdSampleTableViewCell,
                         cellCount: 5,
                         cellModelCommon:SampleTableViewCellModel(TitleText: "Sample Data"))
-                    ], sectionHeader: "Section 2", sectionFooter: "Footer 2")];
+                    ], sectionHeader: "Section 2")];
             
             listingVC.spListingSectionDataArray = spListingSectionDataArray
             
-            var tableViewFrame = self.view.frame
-            tableViewFrame.origin.y = 20
-            tableViewFrame.size.height -= 20
-            SPListingHelper.add(child: listingVC, into: self, atPosition: tableViewFrame)
-
+            SPListingHelper.add(child: listingVC, into: self, atPosition: self.view.frame)
         }
     }
 }
