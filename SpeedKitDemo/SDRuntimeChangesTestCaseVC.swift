23348f7f40593508ff1fbec27e6d50f4679d0dc0
Pradip Vaghasiya <pradip.vaghasiya@gmail.com>
2015-04-16 10:56:45 +0530

    Clean Code

--- SpeedKitDemo/SDRuntimeChangesTestCaseVC.swift
+++ SpeedKitDemo/SDRuntimeChangesTestCaseVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+///TODO: Add changes test cases with multiple section and multiple cell Data Sets
 ///Storyboard Segue id for segue to this controller
 let kSegueToSDRuntimeChangesTestCaseVC = "Show SDRuntimeChangesTestCaseVC"
 
@@ -46,7 +48,8 @@ extension SDRuntimeChangesTestCaseVC : UITableViewDelegate{
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
+        self.editItem(indexPath)
+        spTableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
     }
 }
 
@@ -54,14 +57,63 @@ extension SDRuntimeChangesTestCaseVC : UITableViewDelegate{
 extension SDRuntimeChangesTestCaseVC{
     
     @IBAction func addItem(sender: AnyObject) {
+        spTableView.insertRowsAtIndexPaths([self.insertItem("Row Detail")], withRowAnimation: UITableViewRowAnimation.Automatic)
+    }
+    
+    @IBAction func reduceItem(sender: AnyObject) {
+        if let indexPath = self.removeItem(){
+            spTableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }else{
+            SPUIHelper.showWarningAlert(WithMessage: "No Items", OnViewController: self)
+        }
+    }
+    
+    
+    ///TODO: How to unit test private functions
+    ///Inserts item in first cellData types in first section SPListingData.
+    ///
+    ///:param: item Item needs to be added.
+    ///
+    ///:returns: NSIndexPath where the item added.
+    private func insertItem(item: String) -> NSIndexPath{
+        ///TODO: Check how to get addrress reference of cellModel in order to make direct changes.
         var cellModel = spListingData.spListingSectionDataArray[0].cellDataArray[0].cellModel
-        cellModel.append(SPTitleLabelCellModel(TitleText: "Row Detail"))
+        cellModel.append(SPTitleLabelCellModel(TitleText: item))
         spListingData.spListingSectionDataArray[0].cellDataArray[0].cellModel = cellModel
-//        self.tableView?.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.Automatic)
-        spTableView.insertRowsAtIndexPaths([NSIndexPath(forRow: cellModel.count - 1, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Automatic)
+        
+        return NSIndexPath(forRow: cellModel.count - 1, inSection: 0)
+    }
+    
+    ///Removes last item in first cellData types in first section SPListingData.
+    ///
+    ///:returns: NSIndexPath? where the item added. nil if no data.
+    private func removeItem() -> NSIndexPath?{
+        var cellModel = spListingData.spListingSectionDataArray[0].cellDataArray[0].cellModel
+        
+        if cellModel.count == 0{
+            return nil
+        }
+        
+        cellModel.removeLast()
+        
+        spListingData.spListingSectionDataArray[0].cellDataArray[0].cellModel = cellModel
+        
+        return NSIndexPath(forRow: cellModel.count, inSection: 0)
+        
     }
 
-    @IBAction func reduceItem(sender: AnyObject) {
+    ///Edits item at given indexPath
+    ///
+    ///:param: indexPAth Item needs to be edited.
+    private func editItem(indexPAth: NSIndexPath){
+        ///Gets CellData Set and exact index of it.
+        if let (spListingCellData, index) = spListingData.getCellDataDictionary(IndexPath: indexPAth){
+            // If it is of type SPTitleLabelCellModel then change it.
+            if let spTitleLabelCellModel = spListingCellData.cellModel[index] as? SPTitleLabelCellModel{
+                spTitleLabelCellModel.titleText = "RowDetail Edited"
+            }
+        }
     }
+
 
 }
