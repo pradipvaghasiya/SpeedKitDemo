395f99ebaf4546da12e417e5b594f5d92d890adf
Pradip Vaghasiya <pradip.vaghasiya@gmail.com>
2015-04-19 19:39:07 +0530

    Clean Code

--- SpeedKitDemoTests/SPListingCellDataSetTests.swift
+++ SpeedKitDemoTests/SPListingCellDataSetTests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class SPListingCellDataSetTests: XCTestCase {
+   var spListingCellDataSetWithAllDetails : SPListingCellDataSet?
    
    var emptyCellModel : [AnyObject] = []
    var cellModel : [AnyObject] = ["model1","model2"]
@@ -24,9 +25,16 @@ class SPListingCellDataSetTests: XCTestCase {
    override func setUp() {
       super.setUp()
       
+      spListingCellDataSetWithAllDetails = SPListingCellDataSet(
+         cellId: validNibId,
+         cellModelCommon: cellCommonModel,
+         cellModel: cellModel,
+         cellViewType: .NibCell)
+      
    }
    
    override func tearDown() {
+      
       // Put teardown code here. This method is called after the invocation of each test method in the class.
       super.tearDown()
    }
@@ -71,7 +79,7 @@ class SPListingCellDataSetTests: XCTestCase {
    func testPositiveCellCountValue(){
       var spListingCellDataSet = SPListingCellDataSet(cellId: validNibId, cellCount: 10, cellModelCommon: cellCommonModel)
       
-      XCTAssert(spListingCellDataSet.cellCount == 10, "Cellcount should be zero")
+      XCTAssert(spListingCellDataSet.cellCount == 10, "Cellcount should be 10.")
    }
 
    func testZeroCellCountValue(){
@@ -86,7 +94,7 @@ class SPListingCellDataSetTests: XCTestCase {
       XCTAssert(spListingCellDataSet.cellModelArray.count == 2 &&
          spListingCellDataSet.cellId == validNibId &&
          spListingCellDataSet.cellCount == UInt(cellModel.count) &&
-         spListingCellDataSet.cellViewType == .NibCell, "SPListingCellDataSet Created properly")
+         spListingCellDataSet.cellViewType == .NibCell, "SPListingCellDataSet Should be Created properly")
 
    }
    
@@ -97,7 +105,7 @@ class SPListingCellDataSetTests: XCTestCase {
          spListingCellDataSet.cellId == validNibId &&
          spListingCellDataSet.cellViewType == .NibCell &&
          spListingCellDataSet.cellCount == 5 &&
-         (spListingCellDataSet.cellCommonModel! as? String) == cellCommonModel, "SPListingCellDataSet Created properly")
+         (spListingCellDataSet.cellCommonModel! as? String) == cellCommonModel, "SPListingCellDataSet Should be Created properly")
    }
    
    func testBothCellModelAndCellCommonModel(){
@@ -107,7 +115,7 @@ class SPListingCellDataSetTests: XCTestCase {
          spListingCellDataSet.cellId == validNibId &&
          spListingCellDataSet.cellViewType == .PrototypeCell &&
          spListingCellDataSet.cellCount == UInt(cellModel.count) &&
-         (spListingCellDataSet.cellCommonModel! as? String) == cellCommonModel, "SPListingCellDataSet Created properly")
+         (spListingCellDataSet.cellCommonModel! as? String) == cellCommonModel, "SPListingCellDataSet Should be Created properly")
 
    }
    
@@ -118,8 +126,127 @@ class SPListingCellDataSetTests: XCTestCase {
          spListingCellDataSet.cellId == validNibId &&
          spListingCellDataSet.cellViewType == .PrototypeCell &&
          spListingCellDataSet.cellCount == 0 &&
-         (spListingCellDataSet.cellCommonModel! as? String) == "", "SPListingCellDataSet Created properly")
+         (spListingCellDataSet.cellCommonModel! as? String) == "", "SPListingCellDataSet Should be Created properly")
       
    }
+   
+   
+   // MARK: Cell Data Set At Runtime
+   func testUpdateCellIdWithInvalidId(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         cellData.cellId = inValidCellId
+         XCTAssertNil(cellData.cellId, "CellDataSet Cell Id should be nil.")
+      }
+   }
+   
+   func testUpdateCellIdWithValidId(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         cellData.cellId = validNibId
+         XCTAssertNotNil(cellData.cellId, "CellDataSet Cell Id should not be nil.")
+      }
+   }
+   
+   func testAppendCellModelDirectly(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         cellData.cellModelArray.append("model3")
+         XCTAssert(cellData.cellModelArray.count == 3 &&
+            cellData.cellCount == 3 &&
+            (cellData.cellModelArray.last as? String) == "model3", "SPListingCellDataSet Should be valid after update")
+      }
+   }
+
+   func testRemoveCellModelDirectly(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         cellData.cellModelArray.removeLast()
+         XCTAssert(cellData.cellModelArray.count == 1 &&
+            cellData.cellCount == 1 &&
+            (cellData.cellModelArray.last as? String) == "model1", "SPListingCellDataSet Should be valid after update")
+      }
+   }
+   
+   func testEditCellModelDirectly(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         cellData.cellModelArray[1] = "model2 edited"
+         XCTAssert(cellData.cellModelArray.count == 2 &&
+            cellData.cellCount == 2 &&
+            (cellData.cellModelArray.last as? String) == "model2 edited", "SPListingCellDataSet Should be valid after update")
+      }
+   }
+   
+   func testAppendCellModelCopying(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         var cellModelArrayCopy = cellData.cellModelArray
+         cellModelArrayCopy.append("model3")
+         cellData.cellModelArray = cellModelArrayCopy
+         XCTAssert(cellData.cellModelArray.count == 3 &&
+            cellData.cellCount == 3 &&
+            (cellData.cellModelArray.last as? String) == "model3", "SPListingCellDataSet Should be valid after update")
+      }
+   }
+   
+   func testRemoveCellModelCopying(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         var cellModelArrayCopy = cellData.cellModelArray
+         cellModelArrayCopy.removeLast()
+         cellData.cellModelArray = cellModelArrayCopy
+
+         XCTAssert(cellData.cellModelArray.count == 1 &&
+            cellData.cellCount == 1 &&
+            (cellData.cellModelArray.last as? String) == "model1", "SPListingCellDataSet Should be valid after update")
+      }
+   }
+   
+   func testEditCellModelCopying(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         var cellModelArrayCopy = cellData.cellModelArray
+         cellModelArrayCopy[1] = "model2 edited"
+         cellData.cellModelArray = cellModelArrayCopy
+
+         XCTAssert(cellData.cellModelArray.count == 2 &&
+            cellData.cellCount == 2 &&
+            (cellData.cellModelArray.last as? String) == "model2 edited", "SPListingCellDataSet Should be valid after update")
+      }
+   }
+   
+   func testEmptyCellModel(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         cellData.cellModelArray.removeAll(keepCapacity: false)
+         
+         XCTAssert(cellData.cellModelArray.count == 0 &&
+            cellData.cellCount == 0, "SPListingCellDataSet Should be valid after update")
+      }
+   }
+   
+   func testfillCellModel(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         cellData.cellModelArray.removeAll(keepCapacity: false)
+         cellData.cellModelArray = cellModel
+         XCTAssert(cellData.cellModelArray.count == 2 &&
+            cellData.cellCount == 2, "SPListingCellDataSet Should be valid after update")
+      }
+   }
+
+   func testEmptyCellCommonModel(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         cellData.cellCommonModel = nil
+         XCTAssert(cellData.cellCommonModel == nil, "SPListingCellDataSet Should be valid after update")
+      }
+   }
+   
+   func testFillCellCommonModel(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         cellData.cellCommonModel = nil
+         cellData.cellCommonModel = cellCommonModel
+         XCTAssert((cellData.cellCommonModel as? String) == "Common Model", "SPListingCellDataSet Should be valid after update")
+      }
+   }
+   
+   func testUpdateCellCountToZero(){
+      if let cellData = spListingCellDataSetWithAllDetails {
+         cellData.cellCount = 0
+         XCTAssert(cellData.cellCount == 2, "SPListingCellDataSet Should be valid after update")
+      }
+   }
+
    
 }
